1dd306257efb9b1e9c4233a9b8d0fa8aa30218a9
mcm_dev <gtsflyer@gmail.com>
2022-04-02 12:28:52 -0400

    updating index to use div

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card } from 'react-bootstrap';
 
 function App() {
   return (
     <div class="container">
       <div class="row">
         <div class="col">
-          <div class="card">
+          <div class="card" style={{ width: '18rem' }}>
+            <div class="card-img" variant="top" style={{ width: '12rem' }} src="Recipe.png" />
             <div class="card-body">
               <div class="card-title">Edit Recipes</div>
-                <div class="card-text">
+              <div class="card-text">
                 Use available items to create a recipe.
+                <a href="/recipeList" class="stretched-link"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style={{ width: '18rem' }}>
+            <div class="card-img" variant="top" style={{ width: '18rem' }} src="Menu.png" />
+            <div class="card-body">
+              <div class="card-title"e>Edit Menu</div>
+              <div class="card-text">
+                Select specific recipe to be served for meals.
+                <a href="/menuList" class="stretched-link"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style={{ width: '18rem' }}>
+            <div class="card-img" variant="top" style={{ width: '15rem' }} src="Reports.jpg" />
+            <div class="card-body">
+              <div class="card-title">Run Reports</div>
+              <div class="card-text">
+                Create and print useful reports.
+                <a href="/reports" class="stretched-link"></a>
               </div>
             </div>
           </div>
