fa2ac2f77ed66f8a12f64e19bc686530191e4a40
mcm_dev <gtsflyer@gmail.com>
2022-04-02 12:22:03 -0400

    updating index for testing 4

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card } from 'react-bootstrap';
 
 function App() {
   return (
     <div class="container">
       <div class="row">
         <div class="col">
-          <Card style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>Edit Recipes</Card.Title>
-              <Card.Text>
+          <div class="card">
+            <div class="card-body">
+              <div class="card-title">Edit Recipes</div>
+                <div class="card-text">
                 Use available items to create a recipe.
-                <a href="/recipeList" class="stretched-link"></a>
-              </Card.Text>
-            </Card.Body>
-          </Card>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
