64bd29f450f628c62f8e55f41e73d83fc90b60cf
mcm_dev <gtsflyer@gmail.com>
2022-04-02 13:16:35 -0400

    more testing 2

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -5,8 +5,46 @@ import { Card } from 'react-bootstrap';
 function App() {
   return (
     <div class="container">
-      Hello world
+      <div class="row">
+        <div class="col">
+          <div class="card" style={{ width: '18rem' }}>
+            <div class="card-img" variant="top" style={{ width: '12rem' }} src="Recipe.png" />
+            <div class="card-body">
+              <div class="card-title">Edit Recipes</div>
+              <div class="card-ext">
+                Use available items to create a recipe.
+                <a href="/recipeList" class="stretched-link"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style={{ width: '18rem' }}>
+            <div class="card-img" variant="top" style={{ width: '18rem' }} src="Menu.png" />
+            <div class="card-body">
+              <div class="card-title">Edit Menu</div>
+              <div class="card-ext">
+                Select specific recipe to be served for meals.
+                <a href="/menuList" class="stretched-link"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" style={{ width: '18rem' }}>
+            <div class="card-img" variant="top" style={{ width: '15rem' }} src="Reports.jpg" />
+            <div class="card-body">
+              <div class="card-title">Run Reports</div>
+              <div class="card-ext">
+                Create and print useful reports.
+                <a href="/reports" class="stretched-link"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    
   );
 }
 
