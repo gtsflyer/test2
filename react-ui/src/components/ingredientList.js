311bef56c8fa86e970de497a8053dd6a83d9b00e
mcm_dev <gtsflyer@gmail.com>
2023-05-04 21:15:37 -0400

    fix another error

--- react-ui/src/components/ingredientList.js
+++ react-ui/src/components/ingredientList.js
@@ -64,7 +64,7 @@ function Ingredients() {
       ingredientListItem.isDelivered = "No";
 
       // This will send a post request to update the data in the database.
-      fetch(`${process.env.REACT_APP_BASE_URL_LOCAL}/updateIngredient/${ingredientList._id}`, {
+      fetch(`${process.env.REACT_APP_BASE_URL_LOCAL}/updateIngredient/${ingredientListItem._id}`, {
         method: "POST",
         body: JSON.stringify(ingredientListItem),
         headers: {
